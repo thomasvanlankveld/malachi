5e815a86fe59d30d7e138f436462fe0e17176465
Thomas van Lankveld <thomas@cell-0.com>
2015-11-06 17:17:05 +0100

    All sorts of stuff

---
+++ README.md
@@ -0,0 +1,11 @@
+Malachi
+=======
+
+Tiny event dispatcher factory for javascript DDD applications
+
+![Malachi holding a message scroll](https://upload.wikimedia.org/wikipedia/commons/3/39/Duccio_di_Buoninsegna_066.jpg)
+
+Status
+------
+
+This package doesn't do anything yet.
---
+++ malachi.js
@@ -0,0 +1,84 @@
+
+/**
+ * Event dispatcher factory
+ *
+ * Although you should generally avoid multiple dispatchers in the same
+ * project, it may be useful in case there are multiple bounded contexts
+ * within the same project.
+ *
+ * @returns {malachi} The dispatcher factory
+ */
+malachi = (function(_){
+
+  /**
+   * Get the name of an event signature
+   *
+   * @param {function} signature
+   * @returns {string}
+   */
+  function nameOf(signature) {
+    if(typeof signature !== "function") {
+      throw new TypeError("Event signature must be a function");
+    }
+    var name = signature.toString();
+    name = name.substr('function '.length);
+    name = name.substr(0, name.indexOf('('));
+    if(name === "") {
+      throw new Error("Event signature must be a named function");
+    }
+    return name;
+  }
+
+  /**
+   * Factory returning a dispatcher instance
+   *
+   * @returns {object}
+   */
+  return function() {
+
+    /**
+     * A map of event signatures and their registered handlers
+     *
+     * @type {object}
+     */
+    var handlers = {};
+
+    return {
+
+      /**
+       * Register a handler to an event signature
+       *
+       * Multiple handlers can be registered to the same signature. Returns
+       * the dispatcher instance for chaining.
+       *
+       * @param signature
+       * @param handler
+       * @returns {object}
+       */
+      on(signature, handler) {
+        type = nameOf(signature);
+        if(typeof handlers[type] === "undefined") {
+          handlers[type] = [];
+        }
+        handlers[type].push(handler);
+        return this;
+      },
+
+      /**
+       * Fire an event
+       *
+       * Calls all handlers registered to the event signature with this event's
+       * data.
+       *
+       * @param signature
+       * @param event
+       */
+      fire(signature, event) {
+        var type = nameOf(signature);
+        _.each(handlers[type], function(handler) {
+          handler(event);
+        });
+      }
+    };
+  }
+}(_));
