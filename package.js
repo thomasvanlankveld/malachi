501a006d865af520f6217e8282cf7f9659edf212
Thomas van Lankveld <thomas@cell-0.com>
2015-11-13 12:18:25 +0100

    Updated some test-related stuff

--- package.js
+++ package.js
@@ -8,17 +8,15 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
-  api.use([
-    'ecmascript',
-    'underscore'
-  ]);
+  api.use('ecmascript');
   api.addFiles('malachi.js');
   api.export('malachi');
 });
 
-//Package.onTest(function(api) {
-//  api.use('ecmascript');
-//  api.use('tinytest');
-//  api.use('thomasvanlankveld:malachi');
-//  api.addFiles('malachi-tests.js');
-//});
+Package.onTest(function(api) {
+  api.use([
+    'ecmascript',
+    'sanjo:jasmine@0.20.2',
+    'thomasvanlankveld:malachi'
+  ]);
+});
---
+++ tests/server/malachi-spec.js
@@ -0,0 +1,5 @@
+describe('Something something something darkside', () => {
+  it('something something something complete', () => {
+    expect(true).toEqual(true);
+  });
+});
